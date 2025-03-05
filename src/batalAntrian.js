0f441eefba5b831616acc66901dcc78a6b1211d1
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2025-03-05 11:01:31 +0800

    First Init

---
+++ src/batalAntrian.js
@@ -0,0 +1,78 @@
+// PASIEN SUDAH CHECK IN & LEWAT HARI ANTRIAN BELUM ADA
+// import { createHmac, createDecipheriv, createHash } from "crypto";
+// import fetch from "node-fetch";
+import { LZString } from 'lz-string';
+// import LZString from "lz-string";
+// import fetch from "node-fetch";
+import CryptoJS from 'crypto-js';
+import  fetch  from 'node-fetch';
+
+// const bookingId = "250205HR9Y89"; // diisi nomor booking yang akan dibatalkan
+
+
+async function postData() {
+    const consid = "30083";
+    const password = "0jWEBC4A21";
+    const timestamp = Math.floor(Date.now() / 1000);
+    const dataSignature = consid + "&" + timestamp;
+
+    // let dataSignatureHash = createHmac("sha256", password)
+    //     .update(dataSignature)
+    //     .digest();
+
+    // const signature = new Buffer.from(dataSignatureHash).toString("base64");
+
+    // Use crypto-js for HMAC-SHA256
+    const dataSignatureHash = CryptoJS.HmacSHA256(dataSignature, password);
+    const signature = CryptoJS.enc.Base64.stringify(dataSignatureHash);
+
+    const passphrase = consid + password + timestamp;
+
+    const url = "https://apijkn.bpjs-kesehatan.go.id/antreanrs/antrean/batal";
+
+    let valueRequest = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "X-cons-id": consid,
+            "X-Timestamp": timestamp,
+            "X-Signature": signature,
+            User_Key: "63e8b4496f7c6d94ecb570179aea3cf0",
+        },
+        body: JSON.stringify({
+            kodebooking: bookingId,
+            keterangan: "Batal",
+        }),
+    };
+
+    let response = await fetch(url, valueRequest);
+    let json = await response.json();
+    console.log("Response :" + JSON.stringify(json));
+
+    let jsonObj = JSON.parse(JSON.stringify(json));
+
+    //   const txtenctypted = jsonObj.response;
+
+    //   let txtdecrypted = decrypt(txtenctypted, passphrase);
+
+    //   let txtDecompressed =
+    //     LZString.decompressFromEncodedURIComponent(txtdecrypted);
+    //   console.log("==============================");
+    //   console.log("Decrypted Response : " + txtDecompressed);
+}
+
+function decrypt(encrypted, passphrase) {
+    let key = createHash("sha256").update(passphrase).digest();
+    let iv = key.slice(0, 16);
+
+    let decipher = createDecipheriv(
+        "aes-256-cbc",
+        Buffer.from(key, "base64"),
+        Buffer.from(iv, "base64")
+    );
+    let decrypted = decipher.update(Buffer.from(encrypted, "base64"));
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString("utf8");
+}
+
+export {postData}
---
+++ src/main2.js
@@ -0,0 +1,105 @@
+import './style.css'
+import { postData } from './batalAntrian.js'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+
+document.querySelector('#app').innerHTML = `
+            <img
+                src="/images/banner.png"
+                alt="RSI Sultan Agung Banjarbaru"
+                class="rounded mx-auto d-block mb-3 mt-5" />
+            <div class="border shadow-sm rounded">
+                <div class="p-3">
+                    <p class="d-flex justify-content-around">
+                        <a
+                            class="btn btn-danger"
+                            data-bs-toggle="collapse"
+                            href="#multiCollapseExample1"
+                            role="button"
+                            aria-expanded="false"
+                            aria-controls="multiCollapseExample1"
+                            >Pembatalan Kode Booking MJKN</a
+                        >
+                        <button
+                            class="btn btn-warning"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#multiCollapseExample2"
+                            aria-expanded="false"
+                            aria-controls="multiCollapseExample2">
+                            Pembatalan Antrian MJKN
+                        </button>
+                        <button
+                            class="btn btn-info"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target=".multi-collapse"
+                            aria-expanded="false"
+                            aria-controls="multiCollapseExample1 multiCollapseExample2">
+                            Buka kedua Pembatalan
+                        </button>
+                    </p>
+                </div>
+                <div class="row px-5 pb-5">
+                    <div class="col">
+                        <div class="collapse multi-collapse" id="multiCollapseExample1">
+                            <div class="card card-body">
+                                <h2 class="text-center">Pembatalan Kode Booking MJKN</h2>
+                                <form action="">
+                                    <input
+                                        type="text"
+                                        id="bookingId"
+                                        placeholder="Masukkan Kode Booking..."
+                                        class="form-control my-3 py-3" />
+                                    <button
+                                        type="button"
+                                        class="btn btn-danger"
+                                        id="bookingButton">
+                                        Batalkan
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="collapse multi-collapse" id="multiCollapseExample2">
+                            <div class="card card-body">
+                                <h2 class="text-center">Pembatalan Antrian MJKN</h2>
+                                <form action="">
+                                    <input
+                                        type="text"
+                                        id="kodeantrian"
+                                        placeholder="Masukkan Kode Antrian..."
+                                        class="form-control my-3 py-3" />
+                                    <button
+                                        type="button"
+                                        class="btn btn-danger"
+                                        id="kodeantrianbutton">
+                                        Batalkan
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="mt-5 text-black text-center fs-5 mt-5">
+                <p>
+                    Copyright @2025 All Right Reserved – IT RSI Sultan Agung Banjarbaru
+                </p>
+            </footer>
+        <!-- LZString CDN -->
+        <!-- <script src="https://cdn.jsdelivr.net/npm/lz-string@1.4.4/libs/lz-string.min.js"></script> -->
+
+        <!-- <script type="module">
+            import {batalAntrianWeb} from "./batalAntrianWeb.js";
+            document.getElementById("bookingButton").addEventListener("click", batalAntrianWeb);
+        </script> -->
+
+`
+
+// setupCounter(document.querySelector('#counter'))
+postData(document.querySelector('#bookingId'));
+
